c06b99ced7db819a67989bc10b415b2ef0de71c0
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-19 15:26:12 +0900

    価格テスト

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
-    if (isset($_POST['syouhinmei']) == true) {
-        $syouhin = htmlspecialchars($_POST['syouhinmei'], ENT_QUOTES);
-        $firsttime = false;
-    } else {
-        $firsttime = true;
+    if(isset($_POST['syouhinmei']) == true){
+      $syouhin = htmlspecialchars($_POST['syouhinmei'], ENT_QUOTES);
+      $kakaku = $_POST['price'];
+      $firsttime = 0;
+    }elseif($_POST['YesorNo'] == true){
+      $question = $_POST['YesorNo'];
+      $firsttime = 1;
+    }else{
+      $firsttime = 2;
     }
 ?>
 
@@ -11,19 +15,24 @@
 <body>
 
 <?php
-    if ($firsttime == true) {
+    if ($firsttime == 0) {
         print "商品名を入力<br>\n";
         print "<form method=\"post\">\n";
         print "<input type=\"text\" size=\"30\" name=\"syouhinmei\"><br>\n";
+        print "価格を入力<br>\n";
+        print "<form method=\"post\">\n";
+        print "<input type=\"text\" size=\"30\" name=\"price\"><br>\n";
         print "<input type=\"submit\" name=\"status\" value=\"送信\">\n";
         print "</form>\n";
 
-    } else {
+    }elseif($firsttime == 1){
         print $syouhin . "<br><br>";
-        print "もう一度試しますか？<br>\n";
+        print "登録しますか？<br>\n";
         print "<form method=\"post\">\n";
         print "<input type=\"submit\" name=\"status\" value=\"もう一度\">\n";
         print "</form>\n";
+    }elseif($firsttime == 2){
+
     }
 ?>
 
