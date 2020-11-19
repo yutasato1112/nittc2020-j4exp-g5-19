71a45ef42281e8332e7bb61fb158bb354666e9e3
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-19 15:37:38 +0900

    はいいいえテスト

--- index.php
+++ index.php
@@ -27,7 +27,8 @@
         print $kakaku . "<br><br>";
         print "登録しますか？<br>\n";
         print "<form method=\"post\">\n";
-        print "<input type=\"submit\" name=\"status\" value=\"はい\">\n";
+        print "<input type=\"radio\" name=\"userchoice\" value=\"はい\">はい<br>\n";
+        print "<input type=\"radio\" name=\"userchoice\" value=\"いいえ\">いいえ<br>\n";
         print "</form>\n";
     }
 ?>
