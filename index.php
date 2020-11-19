e710692ccdcb2b7ba367a0656b03d441a29e96f2
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-19 15:11:04 +0900

    フォームテスト

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
     if (isset($_POST['syouhinmei']) == true) {
         $syouhin = htmlspecialchars($_POST['syouhinmei'], ENT_QUOTES);
-        $kakaku = htmlspecialchars($_POST['kakaku'], ENT_QUOTES);
         $firsttime = false;
     } else {
         $firsttime = true;
@@ -18,15 +17,9 @@
         print "<input type=\"text\" size=\"30\" name=\"syouhinmei\"><br>\n";
         print "<input type=\"submit\" name=\"status\" value=\"送信\">\n";
         print "</form>\n";
-        print "価格を入力<br>\n";
-        print "<form method=\"post\">\n";
-        print "<input type=\"text\" size=\"30\" name=\"kakaku\"><br>\n";
-        print "<input type=\"submit\" name=\"status\" value=\"送信\">\n";
-        print "</form>\n";
 
     } else {
         print $syouhin . "<br><br>";
-        print $kakaku  . "<br><br>"
         print "もう一度試しますか？<br>\n";
         print "<form method=\"post\">\n";
         print "<input type=\"submit\" name=\"status\" value=\"もう一度\">\n";
