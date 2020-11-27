711bed8e6a54dfe478aa757ca977b6bd83a138e2
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-27 11:02:57 +0900

    pageCの実装

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     if(isset($_POST['syouhinmei']) == true){
       $syouhin = htmlspecialchars($_POST['syouhinmei'], ENT_QUOTES);
       $kakaku = htmlspecialchars($_POST['price'], ENT_QUOTES);
-      pageB();
+      pageC();
       pageB_check();
     }else{
       pageA();
