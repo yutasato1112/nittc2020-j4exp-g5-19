858bdb3abf1fd1ea698965d5b2abfc38c60eb743
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-27 12:50:03 +0900

    ファイル遷移

--- pageA.php
+++ pageA.php
@@ -4,8 +4,9 @@
     print "商品名を入力<br>\n";
     print "<form method=\"post\">\n";
     print "<input type=\"text\" size=\"30\" name=\"syouhinmei\"><br>\n";
+    print "</form>\n";
     print "価格を入力<br>\n";
-    print "<form method=\"post\">\n";
+    print "<form name = \"price\" method=\"post\">\n";
     print "<input type=\"number\" size=\"30\" name=\"price\"><br>\n";
     print "<input type=\"submit\" name=\"statusA\" value=\"送信\">\n";
     print "</form>\n";
@@ -15,7 +16,7 @@
 
 <?php
     if(isset($_POST["statusA"]) ){
-        if(is_int($_POST["price"]) & $_POST["price"] > 0){
+        if(is_int($_POST["price"]) && $_POST["price"] > 0){
             header('Location:pageB.php');
         }else{
             header('Location:pageC.php');
