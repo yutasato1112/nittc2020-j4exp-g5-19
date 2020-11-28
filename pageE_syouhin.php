9d05a199ea914eeaef5337bccdf204a0d1a94bdc
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-29 00:08:20 +0900

    sql

--- pageE_syouhin.php
+++ pageE_syouhin.php
@@ -1,4 +1,6 @@
 DATABASE_URL = postgres://jsavftjpgmyakf:aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b@ec2-52-206-15-227.compute-1.amazonaws.com:5432/d90s2fmuo5249c
+<html>
+<body>
 <?php
 $url = parse_url(getenv('DATABASE_URL'));
 
@@ -6,3 +8,6 @@ $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1))
 
 $pdo = new PDO($dsn, $url['user'], $url['pass']);
 var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
+?>
+</html>
+</body>
