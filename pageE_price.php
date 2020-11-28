97ce52998c8d57197752e55b37d89ec487574e5d
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-29 02:08:14 +0900

    sql

--- pageE_price.php
+++ pageE_price.php
@@ -6,8 +6,8 @@ try {
     $user = aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b;
     $password = jsavftjpgmyakf;
     $dbh = new PDO($dsn, $user, $password);
-    db_access($dbh);
     echo "接続成功: ";
+    getMessage() . "\n";
     } catch (PDOException $e) {
     echo "申し訳ございません。接続失敗: " . $e-
     exit();
