ce5e36f54ad88adaf72e4a3aee38f81b4e57627c
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-29 00:05:06 +0900

    sql

--- pageE_syouhin.php
+++ pageE_syouhin.php
@@ -1,7 +1,6 @@
 DATABASE_URL = postgres://jsavftjpgmyakf:aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b@ec2-52-206-15-227.compute-1.amazonaws.com:5432/d90s2fmuo5249c
 <?php
 $url = parse_url(getenv('DATABASE_URL'));
-$url = parse_url(getenv('DATABASE_URL'));
 
 $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
 
