a9ddb61d51c4e9accbe48a2d2affcc53f977d457
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-29 02:27:58 +0900

    sql

--- pageE_price.php
+++ pageE_price.php
@@ -1,16 +1,14 @@
 <html>
 <body>
 <?php
-try {
-    $dsn = d90s2fmuo5249c;
-    $user = jsavftjpgmyakf;
-    $password = aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b;
-    $dbh = new PDO($dsn, $user, $password);
-    echo "接続成功: ";
-    getMessage() . "\n";
+    $dsn ='pgsql:host=ec2-52-206-15-227.compute-1.amazonaws.com;dbname=d90s2fmuo5249c;port=5432';
+    $user = 'jsavftjpgmyakf';
+    $password = 'aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b';
+    try {
+        $dbh = new PDO($dsn, $user, $password);
     } catch (PDOException $e) {
-    echo "申し訳ございません。接続失敗: " . $e-
-    exit();
+        echo "接続失敗: " . $e-
+        exit();
     }
 ?>
 </html>
--- pageE_syouhin.php
+++ pageE_syouhin.php
@@ -9,7 +9,7 @@
     print "<th>登録日</th>";  // 表の見出し 登録日  
     print "</tr>"; 
 
-    $connect = new PDO("pgsql:host=ec2-52-206-15-227.compute-1.amazonaws.com;dbname=d90s2fmuo5249c;port=5432;user=jsavftjpgmyakf;password=aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b;port=5432");
+    $connect = new PDO("pgsql:host=ec2-52-206-15-227.compute-1.amazonaws.com;dbname=d90s2fmuo5249c;port=5432;user=jsavftjpgmyakf;password=aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b");
     
     $result = $connect->query("SELECT * FROM order");
     $rs = $result->fetchall(); 
