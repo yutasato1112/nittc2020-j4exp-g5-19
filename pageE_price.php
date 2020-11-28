33164df15d94521b3ca6e3edcc2b78f4fded7f55
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-29 01:59:01 +0900

    sql

--- pageE_price.php
+++ pageE_price.php
@@ -2,13 +2,16 @@
 <body>
 <?php
 try {
-$dbh = new PDO($dsn, $user, $password);
-db_access($dbh);
-getMessage() . "\n";
-} catch (PDOException $e) {
-echo "申し訳ございません。接続失敗: " . $e-
-exit();
-}
+    $dsn = d90s2fmuo5249c;
+    $user = aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b;
+    $password = jsavftjpgmyakf;
+    $dbh = new PDO($dsn, $user, $password);
+    db_access($dbh);
+    getMessage() . "\n";
+    } catch (PDOException $e) {
+    echo "申し訳ございません。接続失敗: " . $e-
+    exit();
+    }
 ?>
 </html>
 </body>
