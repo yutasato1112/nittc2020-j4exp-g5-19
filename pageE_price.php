b8e570276f96a6c3bd7d354121e5bb22dc190e53
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-29 01:05:08 +0900

    sql

--- pageE_price.php
+++ pageE_price.php
@@ -1,7 +1,12 @@
 <html>
 <body>
 <?php
-
+print "<table border=1 cellspacing=1 cellpadding=1>\n"; // 表の開始(HTML タグ)  
+print "<tr>";              // 表の見出し行の始まり  
+print "<th>商品名</th>";   // 表の見出し 商品名   
+print "<th>価格</th>";  // 表の見出し 価格  
+print "<th>発売日</th>";  // 表の見出し 発売日  
+print "</tr>"; 
 ?>
 </html>
 </body>
