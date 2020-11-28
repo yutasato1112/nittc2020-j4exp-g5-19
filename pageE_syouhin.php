300bc56d077155406dde9d3a81e69413923f634f
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-29 00:34:30 +0900

    sql

--- pageE_syouhin.php
+++ pageE_syouhin.php
@@ -3,7 +3,10 @@
 <?php
     $connect = new PDO("pgsql:host=ec2-52-206-15-227.compute-1.amazonaws.com;dbname=order;port=5432;user=jsavftjpgmyakf;password=aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b");
     
-
+    $result = $connect->query("SELECT * FROM order");
+    foreach($results as $row) {
+        ?><?=row["item"]?> : <?=row["price"]?><br>
+    }
 ?>
 </html>
 </body>
