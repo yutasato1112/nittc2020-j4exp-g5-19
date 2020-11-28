c229f1065705ac2a74d0de0a24458d7bfd66fc58
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-29 00:17:13 +0900

    sql

--- pageE_syouhin.php
+++ pageE_syouhin.php
@@ -1,25 +1,40 @@
-DATABASE_URL = postgres://jsavftjpgmyakf:aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b@ec2-52-206-15-227.compute-1.amazonaws.com:5432/d90s2fmuo5249c
 <html>
+<head><title>PHP TEST</title></head>
 <body>
+
 <?php
-$url = parse_url(getenv('DATABASE_URL'));
 
-return [
-    'default' => 'pgsql',
+function convert_enc($str){
+    $from_enc = 'UTF-8';
+    $to_enc = 'SJIS';
 
-    'connections' => [
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => $url['host'],
-            'database' =>  substr($url['path'], 1),
-            'username' => $url['user'],
-            'password' => $url['pass'],
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-        ],
-    ],
-];
+    return mb_convert_encoding($str, $to_enc, $from_enc);
+}
+
+$dsn = 'pgsql:dbname=order host=ec2-52-206-15-227.compute-1.amazonaws.com
+port=5432';
+$user = 'jsavftjpgmyakf';
+$password = 'aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b';
+
+try{
+    $dbh = new PDO($dsn, $user, $password);
+
+    print('接続に成功しました。<br>');
+
+    $sql = 'select * from shouhin';
+    foreach ($dbh->query($sql) as $row) {
+        print(convert_enc($row['id']));
+        print(convert_enc($row['name']).'<br>');
+    }
+}catch (PDOException $e){
+    print('Error:'.$e->getMessage());
+    die();
+}
+
+
+$dbh = null;
+
 ?>
-</html>
+
 </body>
+</html>
