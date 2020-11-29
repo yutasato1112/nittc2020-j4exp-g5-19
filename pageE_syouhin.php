0f3f13cbe47d948a1e27683931f31c18d2b2c830
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-30 00:19:33 +0900

    sql条件

--- pageE_syouhin.php
+++ pageE_syouhin.php
@@ -7,6 +7,7 @@
 <body>
 <?php
 try{
+    $syouhin_name = "Dell";
     print "<form method=\"post\">\n";
     print "<input type=\"submit\" name=\"statusE_syouhintoD\" value=\"検索ページへ\">\n";
     print "</form>\n";
@@ -16,8 +17,6 @@ try{
     
     $sql = "SELECT * FROM gadget WHERE 商品名 like '%$syouhin_name%' ORDER BY 登録日 DESC";
     $result = $connect->query($sql);
-    // データベース検索結果の件数を出力   
-    print "件数は".$stmt->rowCount()."です";
     
     print "<h3>デーベース検索結果</h3>\n"; // 表の開始(HTML タグ)
     print "<table border=1 cellspacing=1 cellpadding=1>\n"; // 表の開始(HTML タグ)  
