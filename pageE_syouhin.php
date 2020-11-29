f18a6d80c20ca75f024d82f61bbb95e20bf9a801
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-29 19:47:04 +0900

    sql

--- pageE_syouhin.php
+++ pageE_syouhin.php
@@ -7,7 +7,8 @@
     $password = 'aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b';
     $sql = "select * from order";
     try {
-        $dbh = new PDO($dsn, $user, $password);
+        $dbh = new
+        PDO($dsn, $user, $password);
         echo "接続成功";
         $result = $dbh->query($sql);
 
