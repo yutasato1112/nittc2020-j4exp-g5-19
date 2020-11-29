07dc5ccc126747b04c10843ca787d347d6ef79b2
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-29 19:38:43 +0900

    sql

--- pageE_syouhin.php
+++ pageE_syouhin.php
@@ -2,20 +2,22 @@
 <body>
     
 <?php
-    print "<table border=1 cellspacing=1 cellpadding=1>\n"; // 表の開始(HTML タグ)  
-    print "<tr>";              // 表の見出し行の始まり  
-    print "<th>商品名</th>";   // 表の見出し 商品名   
-    print "<th>価格</th>";  // 表の見出し 価格  
-    print "<th>登録日</th>";  // 表の見出し 登録日  
-    print "</tr>"; 
-
     $dsn ='pgsql:host=ec2-52-206-15-227.compute-1.amazonaws.com;dbname=d90s2fmuo5249c;port=5432';
     $user = 'jsavftjpgmyakf';
     $password = 'aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b';
+    $sql = "select * from order";
     try {
         $dbh = new PDO($dsn, $user, $password);
         echo "接続成功";
-        $result = $dbh->query("SELECT * FROM order");
+        $result = $dbh->query($sql);
+
+        print "<table border=1 cellspacing=1 cellpadding=1>\n"; // 表の開始(HTML タグ)  
+        print "<tr>";              // 表の見出し行の始まり  
+        print "<th>商品名</th>";   // 表の見出し 商品名   
+        print "<th>価格</th>";  // 表の見出し 価格  
+        print "<th>登録日</th>";  // 表の見出し 登録日  
+        print "</tr>"; 
+
         $rs = $result->fetchall; 
         
         foreach($rs as $row):    
