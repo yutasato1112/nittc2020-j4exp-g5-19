a2dac64781a9908410ddebfe33c065e719a1e224
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-29 19:49:29 +0900

    sql

--- pageE_syouhin.php
+++ pageE_syouhin.php
@@ -5,8 +5,8 @@
     $dsn ='pgsql:host=ec2-52-206-15-227.compute-1.amazonaws.com;dbname=d90s2fmuo5249c;port=5432';
     $user = 'jsavftjpgmyakf';
     $password = 'aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b';
-    $sql = "select * from order";
     try {
+        $sql = "select * from order";
         $dbh = new
         PDO($dsn, $user, $password);
         echo "接続成功";
