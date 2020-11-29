f6a8014abc8a46cc1d6677e56106dcc6d8e2bc17
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-30 01:14:02 +0900

    sql条件

--- pageD.php
+++ pageD.php
@@ -25,6 +25,7 @@
 </html>
 </body>
 <?php
+    session_start();
     $_SESSION['syouhin_name']= $_POST["kensakumei"];
     $_SESSION['Low_Price'] = $_POST["low_price"];
     $_SESSION['High_Price'] = $_POST["high_price"];
