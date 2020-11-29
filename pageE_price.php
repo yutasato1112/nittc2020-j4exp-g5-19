1da4d7afe1908eeea7b2f6f899ed593418ae6f5d
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-29 20:29:41 +0900

    sql

--- pageE_price.php
+++ pageE_price.php
@@ -1,23 +1,41 @@
 <html>
 <body>
 <?php
-    $dsn ='pgsql:host=ec2-52-206-15-227.compute-1.amazonaws.com;dbname=d90s2fmuo5249c;port=5432';
-    $user = 'jsavftjpgmyakf';
-    $password = 'aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b';
-    try {
-        $dbh = new PDO($dsn, $user, $password);
-        $sql = "select * from order";
-        echo "接続成功";
-        $result = $dbh->query($sql);
-        $rs = $result->fetchall(); 
-        error_log('$rs:' . get_str_var_dump($rs));
+    $server      = "ec2-52-206-15-227.compute-1.amazonaws.com";
+    $darabase    = "d90s2fmuo5249c";
+    $port_number = "5432";
+    $user_id     = "jsavftjpgmyakf";
+    $user_pass   = "aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b";
 
-        
+    $connect = new
+    PDO("pgsql:host = $server;dbname = $darabase;port = $port_number;user = $user_id;password = $user_pass");
+    $km = 0;
+    $MaxA = 100000;
+    $MinA = 5000;
+    $sql_text = "select * from prod where 価格 between '$MinA' and '$MaxA'　ORDER BY 登録日 DESC";
+    
+    $result = $connect->query($sql_text); 
+    print "<h3>検索結果</h3>\n";
+    print "件数は".$result->rowCount()."件です"; 
+    print "<table border=1 cellspacing=1 cellpadding=1>\n"; 
+      print "<tr>"; 
+      print "<th>商品名</th>"; 
+      print "<th>価格</th>"; 
+      print "<th>登録日</th>"; 
+      print "</tr>"; 
+    $rs = $result->fetchall();
+    foreach($rs as $row):
+        print "<tr>"; 
+        print '<td>'.$row['商品名']."</td>";
+        print '<td>'.$row['価格']."</td>";
+        print '<td>'.$row['登録日']."</td>";
+        print "</tr>\n"; 
+    endforeach;
+    print "</table>\n"; 
+    
 
-    } catch (PDOException $e) {
-        echo "接続失敗: " . $e-
-    exit();
-    }
 ?>
 </html>
 </body>
+
+
