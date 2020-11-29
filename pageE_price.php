71cb2c0d33344e0b032ca25732d23ff1a40cf15b
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-29 20:37:06 +0900

    sql

--- pageE_price.php
+++ pageE_price.php
@@ -7,33 +7,35 @@
     $user_id     = "jsavftjpgmyakf";
     $user_pass   = "aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b";
 
-    $connect = new
-    PDO("pgsql:host = $server;dbname = $darabase;port = $port_number;user = $user_id;password = $user_pass");
-    $km = 0;
-    $MaxA = 100000;
-    $MinA = 5000;
-    $sql_text = "select * from prod where 価格 between '$MinA' and '$MaxA'　ORDER BY 登録日 DESC";
+    $connect = new 
+    PDO("pgsql:host = $server;
+        dbname     = $darabase;
+        port       = $port_number;
+        user       = $user_id;
+        password   = $user_pass");
+    $sql_text = "select * from prod "; 
     
     $result = $connect->query($sql_text); 
     print "<h3>検索結果</h3>\n";
-    print "件数は".$result->rowCount()."件です"; 
+    print "件数は".$result->rowCount()."件です";
+​
     print "<table border=1 cellspacing=1 cellpadding=1>\n"; 
       print "<tr>"; 
       print "<th>商品名</th>"; 
       print "<th>価格</th>"; 
       print "<th>登録日</th>"; 
       print "</tr>"; 
-    $rs = $result->fetchall();
-    foreach($rs as $row):
+​
+      $rs = $result->fetchall();
+      foreach($rs as $row):
         print "<tr>"; 
         print '<td>'.$row['商品名']."</td>";
         print '<td>'.$row['価格']."</td>";
         print '<td>'.$row['登録日']."</td>";
         print "</tr>\n"; 
-    endforeach;
-    print "</table>\n"; 
-    
-
+      endforeach;
+      print "</table>\n"; 
+  }
 ?>
 </html>
 </body>
--- pageE_syouhin.php
+++ pageE_syouhin.php
@@ -6,34 +6,34 @@
     $user = 'jsavftjpgmyakf';
     $password = 'aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b';
     try {
-        $sql = "select * from order";
+        $sql = "SELECT * FROM order LIMIT 50";
         $dbh = new
         PDO($dsn, $user, $password);
         echo "接続成功";
+        $result = $dbh->query($sql);
+
+        print "<table border=1 cellspacing=1 cellpadding=1>\n"; // 表の開始(HTML タグ)  
+        print "<tr>";              // 表の見出し行の始まり  
+        print "<th>商品名</th>";   // 表の見出し 商品名   
+        print "<th>価格</th>";  // 表の見出し 価格  
+        print "<th>登録日</th>";  // 表の見出し 登録日  
+        print "</tr>"; 
+
+        $rs = $result->fetchall(); 
+        
+        foreach($rs as $row):    
+            print "<tr>";              // 表の明細行の始まり    
+            print '<td>'.$row['商品名']."</td>";    
+            print '<td>'.$row['価格']."</td>"; 
+            print '<td>'.$row['登録日']."</td>";    
+            print "</tr>\n";             // 表の明細行の終わり
+        endforeach; print "</table>\n";             // 表の終わり 
+
     } catch (PDOException $e) {
         echo "接続失敗: " . $e-
     exit();
     }
 
-    $result = $dbh->query($sql);
-    print "<table border=1 cellspacing=1 cellpadding=1>\n"; // 表の開始(HTML タグ)  
-    print "<tr>";              // 表の見出し行の始まり  
-    print "<th>商品名</th>";   // 表の見出し 商品名   
-    print "<th>価格</th>";  // 表の見出し 価格  
-    print "<th>登録日</th>";  // 表の見出し 登録日  
-    print "</tr>"; 
-
-    $rs = $result->fetchall(); 
-        
-    foreach($rs as $row):    
-        print "<tr>";              // 表の明細行の始まり    
-        print '<td>'.$row[0]."</td>";    
-        print '<td>'.$row[1]."</td>"; 
-        print '<td>'.$row[2]."</td>";    
-        print "</tr>\n";             // 表の明細行の終わり
-    endforeach; print "</table>\n";             // 表の終わり 
-    var_dump($result);
-    var_dump($rs);
 
 
     
