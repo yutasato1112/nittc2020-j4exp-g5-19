b52b7574e3ffc6219cc4201d715ada47ba738cc6
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-29 20:53:06 +0900

    sql

--- pageE_syouhin.php
+++ pageE_syouhin.php
@@ -1,43 +1,13 @@
 <html>
 <body>
-    
 <?php
-    $dsn ='pgsql:host=ec2-52-206-15-227.compute-1.amazonaws.com;dbname=d90s2fmuo5249c;port=5432';
-    $user = 'jsavftjpgmyakf';
-    $password = 'aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b';
-    try {
-        $sql = "SELECT (*) FROM 'order' LIMIT 50";
-        $dbh = new
-        PDO($dsn, $user, $password);
-        echo "接続成功";
-        $result = $dbh->query($sql);
-
-        print "<table border=1 cellspacing=1 cellpadding=1>\n"; // 表の開始(HTML タグ)  
-        print "<tr>";              // 表の見出し行の始まり  
-        print "<th>商品名</th>";   // 表の見出し 商品名   
-        print "<th>価格</th>";  // 表の見出し 価格  
-        print "<th>登録日</th>";  // 表の見出し 登録日  
-        print "</tr>"; 
-
-        $rs = $result->fetchall(); 
-        
-        foreach($rs as $row):    
-            print "<tr>";              // 表の明細行の始まり    
-            print '<td>'.$row['商品名']."</td>";    
-            print '<td>'.$row['価格']."</td>"; 
-            print '<td>'.$row['登録日']."</td>";    
-            print "</tr>\n";             // 表の明細行の終わり
-        endforeach; print "</table>\n";             // 表の終わり 
-
-    } catch (PDOException $e) {
-        echo "接続失敗: " . $e-
-    exit();
+    $connect = new
+    PDO("pgsql:host=ec2-52-206-15-227.compute-1.amazonaws.com;dbname=d90s2fmuo5249c;port=5432;user=jsavftjpgmyakf;password=aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b");
+    $result = $connect->query("SELECT * FROM order");
+    foreach($results as $row) {
+        ?><?=row["item"]?> : <?=row["price"]?><br>
+        <?php
     }
-
-
-
-    
-    
 ?>
 </html>
-</body>
+<body>
