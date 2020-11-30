11e8666fcc04e2a149c63d109900e95369d8261e
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-11-30 13:35:00 +0900

    sql条件

--- pageD.php
+++ pageD.php
@@ -2,28 +2,28 @@
 <body>
 <?php
 session_start();
-    print "検索条件<br>\n";
-    print "商品名検索<br>\n";
-    print "<form method=\"post\">\n";
-    print "<input type=\"text\" size=\"30\" name=\"kensakumei\"><br>\n";
-    print "<input type=\"submit\" name=\"statusD_syouhin\" value=\"送信\"><br><br>\n\n";
-    print "</form>\n";
+    print '検索条件<br>\n';
+    print '商品名検索<br>\n';
+    print '<form method=\"post\">\n';
+    print '<input type=\"text\" size=\"30\" name=\"kensakumei\"><br>\n';
+    print '<input type=\"submit\" name=\"statusD_syouhin\" value=\"送信\"><br><br>\n\n';
+    print '</form>\n';
 
-    print "価格検索<br?\n";
+    print '価格検索<br?\n';
     print '<form action = "pageE_price".method="get">';
-    print "<input type=\"number\" size=\"30\" name=\"low_price\" >";
-    print "</form>";
-    print "  〜  ";
+    print '<input type=\"number\" size=\"30\" name=\"low_price\" >';
+    print '</form>';
+    print '  〜  ';
     print '<form action = "pageE_price".method="get">';
-    print "<input type=\"number\" size=\"30\" name=\"high_price\"><br>\n";
-    print "</form>";
-    print "<form method=\"post\">";
-    print "<input type=\"submit\" name=\"statusD_price\" value=\"送信\"><br><br><br>\n\n\n";
-    print "</form>\n";
+    print '<input type=\"number\" size=\"30\" name=\"high_price\"><br>\n';
+    print '</form>';
+    print '<form method=\"post\">';
+    print '<input type=\"submit\" name=\"statusD_price\" value=\"送信\"><br><br><br>\n\n\n';
+    print '</form>\n';
 
-    print "<form method=\"post\">\n";
-    print "<input type=\"submit\" name=\"statusDtoA\" value=\"商品登録ページへ\">\n";
-    print "</form>\n";
+    print '<form method=\"post\">\n';
+    print '<input type=\"submit\" name=\"statusDtoA\" value=\"商品登録ページへ\">\n';
+    print '</form>\n';
 ?>
 </body>
 </html>
