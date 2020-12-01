ac1ace6cd8921f036345cc0eda2c9b30019da00a
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-12-01 15:12:59 +0900

    SQL追加テスト

--- pageA.php
+++ pageA.php
@@ -2,7 +2,7 @@
 <body>
 <?php
     print "<h3>商品登録ページ</h3><br>\n";
-    print '<form action="pageB.php" method="get">';
+    print '<form action="testa.php" method="get">';
     print "商品名を入力<br>";
     print '<input type="text"     name="syouhin" value="" size="20" maxlength="20"><br>';
     print "価格を入力<br>";
--- testa.php
+++ testa.php
@@ -11,6 +11,16 @@
     if($sousin == true){
         if ($value == yes) {
             header('Location:pageA.php');
+            $syouhinmei = $_GET['syouhin'];
+            $price = $_GET['kakaku'];
+
+            $timestamp = time();
+            $date = date("Y/m/d", $timestamp);
+            $connect = new
+            PDO("pgsql:host=ec2-52-206-15-227.compute-1.amazonaws.com;dbname=d90s2fmuo5249c;port=5432;user=jsavftjpgmyakf;password=aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b"); 
+            $sql_add = "INSERT INTO gadget (商品名, 価格, 登録日) VALUES ($syouhinmei, $price, $date)";
+            $result = $connect -> quety($sql_add);
+
         }elseif ($value == no){
             header('Location:pageA.php');
         }else{
