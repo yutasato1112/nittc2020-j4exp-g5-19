0373b2d09344c144b9fc590455b423a37e00648b
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-12-01 14:35:24 +0900

    GETテスト

--- testa.php
+++ testa.php
@@ -3,7 +3,7 @@
 <html>
 <body>
 <?php
-    print '<form action="testb.php" method="get">';
+    print '<form action="pageE_price.php" method="get">';
     print '<input type="number"     name="a" value="" size="20" maxlength="20"><br>';
     print '<input type="number"     name="b" value="" size="20" maxlength="20"><br>';
     print ' <input type="submit" />';
