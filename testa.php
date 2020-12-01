fa9e909639bcab78fb30eac21c09ebe28b7e8f8e
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-12-01 15:03:17 +0900

    GETテスト

--- testa.php
+++ testa.php
@@ -1,12 +1,12 @@
 <form>
-    あなたは学生ですか?<br>
-    <input type="radio" name="student" value="学生です">はい
-    <input type="radio" name="student" value="学生ではありません">いいえ
+    登録しますか?<br>
+    <input type="radio" name="YesorNo" value="yes">はい
+    <input type="radio" name="YesorNo" value="no">いいえ
     <br>
     <input type="submit">
 </form>
 <?php
-    $value = $_GET['student'];
+    $value = $_GET['YesorNo'];
     if ($value) {
         echo "私は". $value;
     }
