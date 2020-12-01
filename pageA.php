2c08534ff79b1d728f2b40fec3e4b7a2c573c2ee
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-12-01 14:50:43 +0900

    GETテスト

--- pageA.php
+++ pageA.php
@@ -1,19 +1,9 @@
 <html>
 <body>
 <?php
-    print "商品名を入力<br>\n";
-    print "<form method=\"post\">\n";
-    print "<input type=\"text\" size=\"30\" name=\"syouhinmei\"><br>\n";
-    print "</form>\n";
-    print "価格を入力<br>\n";
-    print "<form name = \"price\" method=\"post\">\n";
-    print "<input type=\"number\" size=\"30\" name=\"price\"><br>\n";
-    print "<input type=\"submit\" name=\"statusA\" value=\"送信\">\n";
-    print "</form>\n";
-
-    print "<h1>商品登録ページ</h1><br>\n";
+    print "<h3>商品登録ページ</h3><br>\n";
     print '<form action="pageB.php" method="get">';
-    print "価格を入力<br>";
+    print "商品名を入力<br>";
     print '<input type="text"     name="syouhin" value="" size="20" maxlength="20"><br>';
     print "価格を入力<br>";
     print '<input type="number"     name="kakaku" value="" size="20" maxlength="20"><br>';
@@ -22,15 +12,3 @@
 ?>
 </html>
 </body>
-
-<?php
-    if(isset($_POST["statusA"]) ){
-        if($_POST["price"] >= 0){
-            $syouhinmei = $_POST['syouhinmei'];
-            $price = $_POST['price'];
-            header('Location:pageB.php');
-        }else{
-            header('Location:pageC.php');
-        }
-    }
-?>
