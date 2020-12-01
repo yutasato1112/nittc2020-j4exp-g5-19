379f8e621fee4d0b81c1135aece35d9bf654ff77
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-12-01 15:08:30 +0900

    GETテスト

--- testa.php
+++ testa.php
@@ -3,13 +3,16 @@
     <input type="radio" name="YesorNo" value="yes">はい
     <input type="radio" name="YesorNo" value="no">いいえ
     <br>
-    <input type="submit">
+    <input type="submit" name="sousin">
 </form>
 <?php
+    $sousin = $_GET['sousin'];
     $value = $_GET['YesorNo'];
-    if ($value == yes) {
-        header('Location:pageA.php');
-    }else{
-        header('Location:pageC.php');
+    if($sousin == true){
+        if ($value == yes) {
+            header('Location:pageA.php');
+        }else{
+            header('Location:pageC.php');
+        }
     }
 ?>
