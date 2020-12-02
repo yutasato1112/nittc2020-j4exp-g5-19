6192fe1e0f50bf673cc92e4d1dd069aedf50763a
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-12-02 21:24:58 +0900

    SQL追加テスト

--- testc.php
+++ testc.php
@@ -11,14 +11,19 @@
         echo "$syouhinmei";
         echo "$price";
 
-
+        $timestamp = time();
+        $date = date("Y/m/d", $timestamp);
+        $connect = new
+        PDO("pgsql:host=ec2-52-206-15-227.compute-1.amazonaws.com;dbname=d90s2fmuo5249c;port=5432;user=jsavftjpgmyakf;password=aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b"); 
+        $sql_add = "INSERT INTO gadget (商品名, 価格, 登録日) VALUES ($syouhinmei, $price, $date)";
+        $result = $connect -> quety($sql_add);
     }else{
         header('Location:pageA.php');
     }
 ?>
-    <p>登録完了</p> 
+    <p>登録完了</p>
     <form action="pageA.php" method="post">
-    <input type="submit" value="送信する">
+    <input type="submit" value="OK">
     </form>
 </body>
 </html>
