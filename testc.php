e0fb58fc2bd977cc72a0442c87e2c21a824f4d66
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2020-12-02 23:55:23 +0900

    SQL追加テスト

--- testc.php
+++ testc.php
@@ -10,9 +10,8 @@
         try{
             $connect = new
             PDO("pgsql:host=ec2-52-206-15-227.compute-1.amazonaws.com;dbname=d90s2fmuo5249c;port=5432;user=jsavftjpgmyakf;password=aa711d82b8c4c7118a5c45c5c6cbfdb66b7a2ff2a3443de400e1532ecc29371b");
-            $sql_text = "INSERT INTO gadget (商品名, 価格, 登録日) values ('$syouhinmei', '$price', '$date')"; 
+            $sql_text = "INSERT INTO gadget (商品名, 価格, 登録日) VALUES ('$syouhinmei', $price, '$date')"; 
             $result = $connect->query($sql_text); 
-            echo "到達";
         }catch(PDOException $e){
             echo '予期しないエラーが発生しました。';
             echo $e->getMessage();
